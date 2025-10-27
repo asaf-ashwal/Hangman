6d70f12f3dc797a3bae2637000a5f01f3e8ec6ce
asaf_ashwal <ashwalasafx@gmail.com>
2025-10-27 09:34:48 +0200

    creat new init function

--- game.py
+++ game.py
@@ -3,5 +3,7 @@ from DB import words
 def word_lodory(words):
     word_loc = words[random.randint(0, len(words))]
     return word_loc
-    
 # print(word_lodory(words))
+    
+def game_init():
+    pass
---
+++ main.py
@@ -0,0 +1,5 @@
+from game import game_init
+if __name__ == "__main__":
+    player_name = "asaf"
+    # input('enter your name')
+    game_init()
